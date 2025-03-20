a62c117d7a1b2aa48f1f91825131858dae1ff258
AkadiG1t <dubkowalex@yandex.ru>
2025-03-20 19:35:28 +0300

    Создать репозиторий на гите

---
+++ src/test/java/ru/spring_pet_project/spring_todo/SpringTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spring_pet_project.spring_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
